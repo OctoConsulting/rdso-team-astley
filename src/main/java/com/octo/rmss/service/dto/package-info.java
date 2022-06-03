808a78e2b1a110398df49ae40cf3f795edbb0021
Jeremy Opacich <opacichjj@gmail.com>
2022-06-03 10:37:04 -0400

    Initial version of rmss generated by generator-jhipster@7.8.1

---
+++ src/main/java/com/octo/rmss/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.octo.rmss.service.dto;
---
+++ src/test/java/com/octo/rmss/web/rest/PublicUserResourceIT.java
@@ -0,0 +1,94 @@
+package com.octo.rmss.web.rest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers.csrf;
+
+import com.octo.rmss.IntegrationTest;
+import com.octo.rmss.config.Constants;
+import com.octo.rmss.config.TestSecurityConfiguration;
+import com.octo.rmss.domain.User;
+import com.octo.rmss.repository.EntityManager;
+import com.octo.rmss.repository.UserRepository;
+import com.octo.rmss.security.AuthoritiesConstants;
+import com.octo.rmss.service.dto.UserDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+/**
+ * Integration tests for the {@link UserResource} REST controller.
+ */
+@AutoConfigureWebTestClient(timeout = IntegrationTest.DEFAULT_TIMEOUT)
+@WithMockUser(authorities = AuthoritiesConstants.ADMIN)
+@IntegrationTest
+class PublicUserResourceIT {
+
+    private static final String DEFAULT_LOGIN = "johndoe";
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private EntityManager em;
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    private User user;
+
+    @BeforeEach
+    public void setupCsrf() {
+        webTestClient = webTestClient.mutateWith(csrf());
+    }
+
+    @BeforeEach
+    public void initTest() {
+        user = UserResourceIT.initTestUser(userRepository, em);
+    }
+
+    @Test
+    void getAllPublicUsers() {
+        // Initialize the database
+        userRepository.create(user).block();
+
+        // Get all the users
+        UserDTO foundUser = webTestClient
+            .get()
+            .uri("/api/users?sort=id,desc")
+            .accept(MediaType.APPLICATION_JSON)
+            .exchange()
+            .expectStatus()
+            .isOk()
+            .expectHeader()
+            .contentType(MediaType.APPLICATION_JSON)
+            .returnResult(UserDTO.class)
+            .getResponseBody()
+            .blockFirst();
+
+        assertThat(foundUser.getLogin()).isEqualTo(DEFAULT_LOGIN);
+    }
+
+    @Test
+    void getAllAuthorities() {
+        webTestClient
+            .get()
+            .uri("/api/authorities")
+            .accept(MediaType.APPLICATION_JSON)
+            .exchange()
+            .expectStatus()
+            .isOk()
+            .expectHeader()
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .expectBody()
+            .jsonPath("$")
+            .isArray()
+            .jsonPath("$[?(@=='" + AuthoritiesConstants.ADMIN + "')]")
+            .hasJsonPath()
+            .jsonPath("$[?(@=='" + AuthoritiesConstants.USER + "')]")
+            .hasJsonPath();
+    }
+}
